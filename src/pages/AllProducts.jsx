eb86b9dd0f801107135d7bbf3927f014fc4f8374
WA-2211 <walaaalghanmi@gmail.com>
2026-08-03 00:03:05 +0300

    update

--- src/pages/AllProducts.jsx
+++ src/pages/AllProducts.jsx
@@ -2,26 +2,35 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import { getAllProducts } from '../services/product-services'
 import { Link } from 'react-router'
-
+import { Flex, Spin } from 'antd'
 
 function AllProducts() {
 
     const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(false)
 
     useEffect(() => {
         async function loadProducts() {
             try {
-
+                setLoading(true)
+                setError(false)
                 const res = await getAllProducts()
                 setProducts(res)
             } catch (err) {
                 console.log(err)
-
+                setError(err.message)
+            }finally{
+                setLoading(false)
             }
         }
         loadProducts()
     }, [])
 
+    if(loading) return <Flex align='center' gap='medium' justify='center'>
+        <Spin size='large' description='Loading...' />
+    </Flex>
+    if(error) return <p>ERROR: {error}</p>
     if (products.length === 0) return <p>No products to show yet</p>
     return (
         <div>
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -3,28 +3,40 @@ import { useState, useEffect } from 'react'
 import { useNavigate, useParams } from 'react-router'
 import { getProductById } from '../services/product-services'
 import { getByIdAndDelete } from '../services/product-services'
-
+import { Flex, Spin } from 'antd'
 
 function ProductDetails() {
 
     const [product, setProduct] = useState(null)
     const { id } = useParams()
     const navigate = useNavigate()
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(false)
+    
 
     useEffect(() => {
         async function loadProductDetails() {
             try {
+                setLoading(true)
+                setError(false)
                 const res = await getProductById(id)
                 setProduct(res)
                 console.log(res)
-            } catch (error) {
-                console.log(error)
+            } catch (err) {
+                console.log(err)
+                setError(err.message)
+            }finally{
+                setLoading(false)
             }
         }
         loadProductDetails()
     }, [])
 
+    if(loading) return <Flex align='center' gap='medium' justify='center'>
+        <Spin size='large' description='Loading...' />
+    </Flex>
 
+    if(error) return <p>ERROR: {error}</p>
     async function handleDelete() {
         try {
             await getByIdAndDelete(id)
